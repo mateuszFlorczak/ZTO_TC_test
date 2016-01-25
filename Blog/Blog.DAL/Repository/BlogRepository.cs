3386b144604630c925e998114e49aa9584c292c2
unknown <student@komp065.nmkzllab>
2016-01-25 13:38:35 +0100

    init commit

---
+++ Blog/Blog.DAL/Repository/BlogRepository.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using Blog.DAL.Infrastructure;
+using Blog.DAL.Model;
+using System;
+
+namespace Blog.DAL.Repository
+{
+    public class BlogRepository
+    {
+        private readonly BlogContext _context;
+
+        public BlogRepository()
+        {
+            _context = new BlogContext();
+        }
+
+        public IEnumerable<Post> GetAllPosts()
+        {
+            return _context.Posts;
+        }
+
+        public void AddPost(Post post)
+        {
+            _context.Posts.Add(post);
+            _context.SaveChanges();
+        }
+    }
+}
